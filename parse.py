8c51a32aef8b28fe802e2f92407c8293b0b8200d
manos89 <mvahlas@gmail.com>
2019-07-21 01:03:31 +0300

    added csv writer

--- parse.py
+++ parse.py
@@ -2,9 +2,9 @@ import requests
 import json
 import argparse
 import datetime
-import pandas as pd
+import csv
 from collections import Counter
-from operator import itemgetter
+
 
 def validate_date(date):
     """
@@ -20,8 +20,10 @@ def validate_date(date):
     except AttributeError:
         print("You must provide a proper date of the following format: YYYY-MM-DD")
 
+
 def format_date(datetime_obj):
     return datetime_obj.strftime("%Y-%m-%dT%H:%M:%SZ")
+
 
 def check_future_date(start_datetime_object, end_datetime_object):
     """
@@ -35,9 +37,11 @@ def check_future_date(start_datetime_object, end_datetime_object):
     if start_datetime_object > end_datetime_object:
         raise ValueError("Your ending date must be after your starting date")
 
+
 def make_request(url):
     r = requests.get(url)
     return json.loads(r.text)
+
 
 def get_args():
     """
@@ -53,9 +57,11 @@ def get_args():
     args = parser.parse_args()
     return args.key, args.start, args.end
 
+
 def create_the_url(key, start, end):
     return "https://app.ticketmaster.com/discovery/v2/events.json?apikey={0}&size=200" \
            "&startDateTime={1}&endDateTime={2}&classificationName=Musician".format(key, start, end)
+
 
 def get_events(data):
     states_dictionary = {}
@@ -75,14 +81,17 @@ def get_artist_with_most_shows(events):
     most_shows_artist = Counter(artists)
     return list(most_shows_artist.keys())[0]
 
+
 def get_venue_with_most_shows(events):
     venues = [artist["_embedded"]["venues"][0]["name"] for artist in events]
     most_shows_venues = Counter(venues)
     return list(most_shows_venues.keys())[0]
 
+
 def most_expensive_ticket(events):
     try:
-        sorted_by_price = sorted([e for e in events if "priceRanges" in e.keys()], key=lambda k: k['priceRanges'][0]["max"],
+        sorted_by_price = sorted([e for e in events if "priceRanges" in e.keys()],
+                                 key=lambda k: k['priceRanges'][0]["max"],
                                  reverse=True)
         return sorted_by_price[0]["name"], sorted_by_price[0]["priceRanges"][0]["max"], \
                sorted_by_price[0]["_embedded"]["attractions"][0]["name"]
@@ -101,12 +110,16 @@ def main():
     url = create_the_url(key, start_formatted, end_formatted)
     data = make_request(url)
     states_dictionary = get_events(data)
+    csvfile = open('results.csv', 'w')
+    spamwriter = csv.writer(csvfile, delimiter=',', lineterminator='\n', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(["State", "Musician with the most events", "Venue with the most events", "Most expensive ticket",
+                     "Most expensive ticket event name", "Most expensive ticket artist"])
     for key in states_dictionary.keys():
         artist = get_artist_with_most_shows(states_dictionary[key])
         venue = get_venue_with_most_shows(states_dictionary[key])
         max_event, max_price, max_artist = most_expensive_ticket(states_dictionary[key])
-        print(key, max_event, max_price, max_artist)
-
+        spamwriter.writerow([key, artist, venue, max_price, max_event, max_artist])
+    csvfile.close()
 
 
 
